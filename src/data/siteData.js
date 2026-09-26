7fa03a90cf784a3f78364df09999bd14961253db
not-asim04 <mr.asim881@gmail.com>
2026-09-26 21:30:39 +0500

    NEWW

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -1,4 +1,4 @@
-import logoImg from '../assets/logo.jpeg'
+import logoImg from '../assets/Logo.jpeg'
 import image4 from '../assets/image4.jpeg'
 import image5 from '../assets/image5.jpeg'
 import image6 from '../assets/image6.jpeg'
